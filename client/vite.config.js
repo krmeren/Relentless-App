8a625e73684ec70427bca51a4a3af21664c2481e
Kerem Eren <78377369+krmeren@users.noreply.github.com>
2026-01-07 21:24:39 +0300

    Fix Vite build path for Plesk

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: path.resolve(__dirname),
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
   server: {
     proxy: {
       '/api': {
