5c5cbe1f2c259710a3eb3003024651e5b07eea3c
Kerem Eren <78377369+krmeren@users.noreply.github.com>
2026-01-07 21:03:39 +0300

    Configure server to host React app

--- server/index.js
+++ server/index.js
@@ -15,6 +15,15 @@ app.use('/api/auth', authRoutes);
 app.use('/api/habits', habitRoutes);
 app.use('/api/users', userRoutes);
 
+// Serve static assets from the client build folder
+const path = require('path');
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// Catch-all route to serve index.html for client-side routing
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
